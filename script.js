aa8191a55f2a318cd1cf82cc06ab364531f72ef9
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-24 16:53:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp','tada','victory','wrong']; // these are filenames without extension
+const buttonContainer = document.getElementById('buttons');
+const audioElements = {};
+
+sounds.forEach((sound) => {
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
+  btn.innerText = sound;
+  btn.addEventListener('click', () => {
+    stopSounds();
+    audioElements[sound].play();
+  });
+
+  const audio = new Audio(`./sounds/${sound}.mp3`);
+  audioElements[sound] = audio;
+
+  buttonContainer.appendChild(btn);
+});
+
+// STOP BUTTON
+const stopBtn = document.createElement('button');
+stopBtn.classList.add('stop');
+stopBtn.classList.add('btn');
+stopBtn.innerText = 'stop';
+stopBtn.addEventListener('click', stopSounds);
+buttonContainer.appendChild(stopBtn);
+
+function stopSounds() {
+  Object.values(audioElements).forEach((audio) => {
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
